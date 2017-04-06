5bebf58710dc1a82c8d647b35ef8304c2d3f9ce1
jumal-capgemini <jumal.ali@capgemini.com>
2017-04-06 11:59:47 +0100

    commented out ajax call for now

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ function logIn(){
      password : document.getElementById("lg_password").value
   }
 
-  	$.ajax({
-  		type: 'POST',
-  		url: 'http://52.213.73.119:80/service/login/auth',
-  		data: JSON.stringify(data), //now data can be added to this function
-  		contentType: "application/json; charset=utf-8",
-  		crossDomain: true,
-  		dataType: "json",
-  		success: function(data) {
+  	//$.ajax({
+  	//	type: 'POST',
+  	//	url: 'http://52.213.73.119:80/service/login/auth',
+  	//	data: JSON.stringify(data), //now data can be added to this function
+  	//	contentType: "application/json; charset=utf-8",
+  	//	crossDomain: true,
+  	//	dataType: "json",
+  	//	success: function(data) {
                 window.location.href="DevOpsCalendarPage";
-                alert("login successful")
-  							console.log(data);
-  							},
-  		error: function(data) { alert('Login failed.');
-  							console.log(data);
-  							}
-  		});
+    //            alert("login successful")
+  	//						console.log(data);
+  	//						},
+  	//	error: function(data) { alert('Login failed.');
+  	//						console.log(data);
+  	//						}
+  	//	});
 }
